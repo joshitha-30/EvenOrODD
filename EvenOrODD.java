1b2e001d53c1222f7ec0c202bce859147f9949e0
joshitha-30 <joshitha301@gmail.com>
2025-01-31 08:47:17 +0530

    Add files via upload

---
+++ EvenOrODD.java
@@ -0,0 +1,9 @@
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        int num = 10; // Replace with any number
+
+        String result = (num % 2 == 0) ? "Even" : "Odd";
+
+        System.out.println(num + " is " + result);
+    }
+} 
